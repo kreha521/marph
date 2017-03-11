a2a04a960bc678670d9becc98852200676584c4c
Masaaki Uegaki <Masaaki Uegaki>
2017-03-12 05:31:23 +0900

    uninstall unrequired packages

--- acl/app.js
+++ acl/app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
@@ -10,8 +9,6 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -19,8 +16,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 var acl = require('./service/acl.js');
 
-acl.isAuthorized('/users/get', ["pm"], (err) => console.log(err));
-acl.isAuthorized('/users/add', ["pm"], (err) => console.log(err));
-acl.isAuthorized('/users/add', ["pm", "admin"], (err) => console.log(err));
+acl.isAuthorized('/users/get', ["pm"], (err) => {
+    if(err) console.log(err)
+});
+acl.isAuthorized('/users/add', ["pm"], (err) => {
+    if(err) console.log(err)
+});
+acl.isAuthorized('/users/add', ["pm", "admin"], (err) => {
+    if(err) console.log(err)
+});
 
 module.exports = app;
