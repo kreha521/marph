eb748b14ef04807d469d067316b3e927be94fb44
Marph <hyyrolll@gmail.com>
2017-04-07 13:14:31 +0900

    dd

---
+++ marph/acl/acl.js
@@ -0,0 +1,39 @@
+var fs = require('fs');
+
+var acl = new Map();
+var settings = JSON.parse(fs.readFileSync(__dirname + '/acl.json', 'utf8'));
+
+for (let setting of settings) {
+    for (let url of Object.keys(setting)) {
+        acl.set(url, new Set(setting[url]));
+    }
+}
+
+module.exports = {
+    isAuthorized:(url, roles, callback) => {
+        if (!url || !Array.isArray(roles)) {
+            callback(new Error('Missing params'));
+            return;
+        }
+
+        if (!acl.has(url)) {
+            callback(new Error('Denied(Url):' + url));
+            return;
+        }
+
+        var isAllowed = false;
+        for (let role of roles) {
+            isAllowed |= (acl.get(url).has(role));
+            if (isAllowed) {
+                break;
+            }
+        }
+
+        if (!isAllowed) {
+            callback(new Error('Denied(Role):' + url));
+            return;
+        }
+
+        callback();
+    }
+}
