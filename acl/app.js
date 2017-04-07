2b2f43d2dddb1e2d50dc8fba18882f7e738a4299
Marph <hyyrolll@gmail.com>
2017-04-07 13:02:12 +0900

    new

---
+++ acl/app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+var acl = require('./acl.js');
+
+acl.isAuthorized('/users/get', ["pm"], (err) => {
+    if(err) console.log(err)
+});
+acl.isAuthorized('/users/add', ["pm"], (err) => {
+    if(err) console.log(err)
+});
+acl.isAuthorized('/users/add', ["pm", "admin"], (err) => {
+    if(err) console.log(err)
+});
+
+module.exports = app;
