3e97a7db2d43f6f787072a3d08caa47ceb5e2d3a
d4c <hyyrolll@gmail.com>
2017-04-27 02:21:45 +0900

    a

--- marbook/src/routes.js
+++ marbook/src/routes.js
@@ -1,9 +1,9 @@
 import * as React from 'react';
 import { BrowserRouter as Router, Route, IndexRoute, browserHistory } from 'react-router-dom'
 import App from './App';
-import Home from './home';
-import Profile from './profile';
-import Works from './works';
+import Home from './component/home';
+import Profile from './component/profile';
+import Works from './component/works';
 
 const router = (
     <Router history={browserHistory}>
